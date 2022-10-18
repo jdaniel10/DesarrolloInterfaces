5986442163d083252ab06b5fdb1cb957868ebd7d
José Daniel Barraza Linares <2021398@iesluisbraille.es>
2022-10-18 09:12:33 +0200

    Trabajo Java Interfaces

---
+++ src/trabajojavainterfaces/TrabajoJavaInterfaces.java
@@ -0,0 +1,17 @@
+
+package trabajojavainterfaces;
+
+/**
+ *
+ * @author JoséDanielBarrazaLin
+ */
+public class TrabajoJavaInterfaces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
